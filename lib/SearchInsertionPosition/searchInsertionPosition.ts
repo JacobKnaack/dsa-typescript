c54a0e3e5c00641f77cc9765e40fe1a973c3b664
JacobKnaack <jacobknck@gmail.com>
2024-07-15 11:41:27 -0700

    adds seachInsertionPosition solution

---
+++ lib/SearchInsertionPosition/searchInsertionPosition.ts
@@ -0,0 +1,37 @@
+/**
+ * Given a sorted array of distinct integers and a target value,
+return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+ * @param {number[]} nums 
+ * @param {number} target
+ * @returns 
+ */
+
+export function searchInsert(nums: number[], target: number) {
+  const search = (left: number, right: number) => {
+    let midIndex = Math.floor((left + right) / 2);
+    let midValue = nums[midIndex];
+    if (midValue === target) {
+      return midIndex;
+    }
+
+    if (target > midValue) {
+      if (left >= right) {
+        return midIndex + 1;
+      }
+      return search(midIndex + 1, right);
+    } else {
+      if (left >= right) {
+        if (midIndex < 0) {
+          return 0;
+        } else {
+          return midIndex;
+        }
+      }
+      return search(left, midIndex - 1);
+    }
+  }
+  return search(0, nums.length -1);
+}
